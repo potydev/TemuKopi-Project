aeb65881f526d0d9e5f1014f8606e16a967cf559
potydev <xysuba0@gmail.com>
2026-06-18 21:18:05 +0700

    gaya: terapkan tata letak flex-column dengan distribusi spasi vertikal ke kontainer MoodFinderPage

--- src/app/pages/MoodFinderPage.tsx
+++ src/app/pages/MoodFinderPage.tsx
@@ -93,7 +93,7 @@ export function MoodFinderPage({
   };
 
   return (
-    <div className="animate-fadeIn">
+    <div className="flex-1 flex flex-col justify-between animate-fadeIn">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-start">
         {/* Left: Chat Container */}
